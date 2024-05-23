2cf2774f3f8c26a7e95c25bbee0e4244e0a97d12
SubnauticaLaserMain <123651736+SubnauticaLaserMain@users.noreply.github.com>
2024-05-23 17:25:10 +0200

    Create main2.lua

---
+++ main2.lua
@@ -0,0 +1,57 @@
+local Workspace = game:GetService('Workspace')
+local Camera = Workspace.CurrentCamera
+local worldToViewportPoint = Camera.worldToViewportPoint
+
+
+shared.VapeIndependent = true
+shared.CustomSaveVape = "name of file to save"
+local GuiLibrary = loadstring(game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua', true))()
+
+
+local Combat = GuiLibrary.ObjectsThatCanBeSaved.CombatWindow.Api
+local Blatant = GuiLibrary.ObjectsThatCanBeSaved.BlatantWindow.Api
+local Render = GuiLibrary.ObjectsThatCanBeSaved.RenderWindow.Api
+local Utility = GuiLibrary.ObjectsThatCanBeSaved.UtilityWindow.Api
+local World = GuiLibrary.ObjectsThatCanBeSaved.WorldWindow.Api
+
+
+
+Render.CreateOptionsButton({
+    Name = 'ItemESP',
+    Function = function(callback)
+        if callback then
+            while callback and task.wait(1) do
+                local a = Workspace:getDescendants()
+                local items = {}
+
+
+                --- First Check
+                for i = 1, #a do
+                    if a[i].Name == 'ItemPickupScript' and a[i].Parent:findFirstChild('ClickDetector') then
+                        table.insert(items, a[i].Parent)
+                    end
+                end
+
+
+
+
+
+
+                for i = 1, #items do
+                    if not items[i]:FindFirstChild('ESP') then
+                        local Hilight = Instance.new('Highlight', items[i])
+
+
+                        Hilight.Name = 'ESP'
+                        Hilight.Adornee = items[i]
+                        Hilight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+                        Hilight.FillTransparency = 0.5
+                        Hilight.FillColor = Color3.new(1, 1, 1)
+                    end
+                end
+            end
+        end
+    end,
+    HoverText = 'Apply ESP to Items',
+    Default = false
+})
